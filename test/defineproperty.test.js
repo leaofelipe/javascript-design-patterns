7b1cf0b7001babb9d6126074b7c7c7bfc4bfe0ee
Felipe Leão <felipe@felipeleao.com>
2017-03-07 21:28:46 -0300

    feat: assign new name to name

--- test/defineproperty.test.js
+++ test/defineproperty.test.js
@@ -2,26 +2,34 @@
 
 const mod = require('../concepts/defineproperty')
 const should = require('chai').should()  // eslint-disable-line no-unused-vars
+const _ = require('underscore')
+const user = mod.user
 
 describe('Define property', () => {
   describe('user', () => {
     let str = 'id: 1, name: Felipe'
 
-    it('user id should be 1', () => {
-      mod.user.id.should.be.equal(1)
+    it('id should be 1', () => {
+      user.id.should.be.equal(1)
     })
 
-    it('user name should be Felipe', () => {
-      mod.user.name.should.be.equal('Felipe')
+    it('name should be Felipe', () => {
+      user.name.should.be.equal('Felipe')
+    })
+
+    it('name should be João', () => {
+      let user = _.clone(mod.user)
+      user.name = 'João'
+      user.name.should.be.equal('João')
     })
 
     it('should be an equal string', () => {
-      mod.user.getAll().should.be.equal(str)
+      user.getAll().should.be.equal(str)
     })
 
     it('should throw error of assign', () => {
       function test () {
-        mod.user.getAll = 1
+        user.getAll = 1
       }
 
       test.should.throw(/Cannot assign to read only/)
